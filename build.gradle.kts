d1384c2a914506f157a7d944586b778940257b1f
davidabsenta <31399650+davidabsenta@users.noreply.github.com>
2026-05-26 20:21:25 -0300

    classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,10 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:8.1.1")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
 
