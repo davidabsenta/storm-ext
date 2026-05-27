77ef70188e17ca03168ea61bf7852762802f5d35
davidabsenta <davidabsenta@gmail.com>
2026-05-27 01:19:48 -0300

    fix: disable broken providers

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,11 @@
 rootProject.name = "CloudstreamPlugins"
 
-// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
-
-val disabled = listOf<String>()
+val disabled = listOf<String>(
+    "AnimeJlProvider",
+    "AnimeflvProvider",
+    "AnimeflvIOProvider",
+    "AnimensionProvider"
+)
 
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
@@ -13,7 +16,3 @@ File(rootDir, ".").eachDir { dir ->
 fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
-
-
-// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-// include("PluginName")
